e232287f4cac6dfcc7fcef05d3572f0ec4c47710
Hillary Hueter <hhueter@gmail.com>
2021-04-29 12:24:19 -0600

    hh - prompt for ttl (#2)
    * hh - prompt for ttl
    
    * hh - default to 48hrs
    
    * Prompt for TTL in days (not hours)
    
    * Collect TTL in days; warn user if quarantine button has a bug
    
    * Fix error reporting
    
    * Remove funding; disable release workflow (for now)
    
    * Fix CSS lint issues
    
    * Fix xo lint issues
    
    Co-authored-by: Tony Spataro <xeger@xeger.net>

--- source/options-storage.js
+++ source/options-storage.js
@@ -3,19 +3,20 @@ import OptionsSync from 'webext-options-sync';
 const EXAMPLE_BODY =
 	`{
 	"context": "{test.context}",
-	"testcase": "{test.testcase}"
+	"testcase": "{test.testcase}", 
+	"ttl_hours": {ttl_hours}
 }`;
 
-const EXAMPLE_QUARANTINE_LIST_URL = `https://webhook.example.com/{vcs.org}/{vcs.repo}/{job.name}/quarantined_tests`;
+const EXAMPLE_QUARANTINE_LIST_URL = 'https://webhook.example.com/{vcs.org}/{vcs.repo}/{job.name}/quarantined_tests';
 
-const EXAMPLE_QUARANTINE_URL = `https://webhook.example.com/{vcs.org}/{vcs.repo}/{job.name}/quarantined_tests/actions/quarantine`
+const EXAMPLE_QUARANTINE_URL = 'https://webhook.example.com/{vcs.org}/{vcs.repo}/{job.name}/quarantined_tests/actions/quarantine';
 
 export default new OptionsSync({
 	defaults: {
 		quarantineListURL: EXAMPLE_QUARANTINE_LIST_URL,
 		quarantineBody: EXAMPLE_BODY,
 		quarantineMethod: 'post',
-		quarantineURL: EXAMPLE_QUARANTINE_URL,
+		quarantineURL: EXAMPLE_QUARANTINE_URL
 	},
 	migrations: [
 		OptionsSync.migrations.removeUnused
